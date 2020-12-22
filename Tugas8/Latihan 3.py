922f707270847714cd3e38c2ee7a3fb7badf2c6a
Bagus Prihandono Pamungkas <71998969+bgspamungkas@users.noreply.github.com>
2020-12-22 22:33:09 +0700

    Update Latihan 3.py

--- Tugas8/Latihan 3.py
+++ Tugas8/Latihan 3.py
@@ -1,18 +1,15 @@
 def bintang(n):
-    
-    x = n//2
-    y = n - x
-    j = 1
-    k = 1 + (x-1)*2
-    l = 1 + x*2
-   
-    for i in range(y):
-        a = "*" * j
-        print(a.center(l))
-        j +=2
-    
-    for i in range(x):
-        b = "*" * k
-        print(b.center(l))
-        k -= 2
+    b = n//2
+    a = n - b
+    g = 1
+    u = 1 + (b-1)*2
+    s = 1 + b*2
+    for i in range(a):
+        n = "*" * g
+        print(n.center(s))
+        g +=2
+    for i in range(b):
+        m = "*" * u
+        print(m.center(s))
+        u -= 2
 bintang(7)
