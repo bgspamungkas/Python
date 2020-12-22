5ba1724f82c5c2bb3afb469da7eccd02ec376607
Bagus Prihandono Pamungkas <71998969+bgspamungkas@users.noreply.github.com>
2020-12-22 22:26:47 +0700

    Add files via upload

--- Tugas8/Latihan 4.py
+++ Tugas8/Latihan 4.py
@@ -1,18 +1,18 @@
 import random
 def shuffleString(x,n):
     kata = list(x)
-    listHasil = []
-    listHasil1 = []
+    listhasil = []
+    listhasil1 = []
     while True:
         random.shuffle(kata) 
         hasil = "".join(kata)
-        a = hasil in listHasil1
-        if a == False:
-            listHasil1 = listHasil1 + [hasil]
-        if len(listHasil1)== n :
-            listHasil = listHasil1
+        b = hasil in listhasil1
+        if b == False:
+            listhasil1 = listhasil1 + [hasil]
+        if len(listhasil1)== n :
+            listhasil = listhasil1
             break
-    return listHasil
+    return listhasil
 print(shuffleString('aku',2))
 print(shuffleString('aku',3))
 print(shuffleString('aku',4))
