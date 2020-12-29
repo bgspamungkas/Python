1d064c05b0960eb11ef7c2b8750a59f88f286821
Bagus Prihandono Pamungkas <71998969+bgspamungkas@users.noreply.github.com>
2020-12-29 10:31:29 +0700

    Update Latihan 7.py

--- Tugas8/Latihan 7.py
+++ Tugas8/Latihan 7.py
@@ -1,19 +1,22 @@
-mhs = ['K001:ROSIHAN ARI:1979-09-01:SOLO',
-        'K002:DWI AMALIA FITRIANI:1979-09-17:KUDUS',
-        'K003:FAZA FAUZAN:2005-01-28:KARANGANYAR']
+mahasiswa = ['K001:ROSIHAN ARI:1979-09-01:SOLO',
+            'K002:DWI AMALIA FITRIANI:1979-09-17:KUDUS',
+            'K003:FAZA FAUZAN:2005-01-28:KARANGANYAR']
+
 print('=' * 70)
 print('NIM'.ljust(12, ' '), end='')
 print('NAMA'.ljust(25, ' '), end='')
 print('TGL LAHIR'.ljust(15, ' '), end='')
 print('TEMPAT LAHIR'.ljust(15))
 print('-' * 70)
-for indeks in range(len(mhs)):
-    data = mhs[indeks]
-    nim, nama, tgl, tmp = data.split(':')
+
+for data in range(len(mahasiswa)):
+    data = mahasiswa[data]
+    nim, nama, tgl, tempat = data.split(':')
     y, m, d = tgl.split('-')
     tanggal = ('/'.join([d, m, y]))
     print(nim.ljust(12, ' '), end='')
     print(nama.ljust(25, ' '), end='')
     print(tanggal.ljust(15, ' '), end='')
-    print(tmp.ljust(15, ' '))
+    print(tempat.ljust(15, ' '))
+    
 print('-' * 70)
