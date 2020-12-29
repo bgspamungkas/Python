ece4cc8a5a4352e3304bf4deeadfcd32031d44c9
Bagus Prihandono Pamungkas <71998969+bgspamungkas@users.noreply.github.com>
2020-12-29 10:06:40 +0700

    Update Latihan 4.py

--- Tugas8/Latihan 4.py
+++ Tugas8/Latihan 4.py
@@ -1,18 +1,23 @@
 import random
-def shuffleString(x,n):
-    kata = list(x)
+def shuffleString(a,c):
+    
+    kata = list(a)
     listhasil = []
     listhasil1 = []
     while True:
+        
         random.shuffle(kata) 
         hasil = "".join(kata)
         b = hasil in listhasil1
+        
         if b == False:
             listhasil1 = listhasil1 + [hasil]
-        if len(listhasil1)== n :
+
+        if len(listhasil1)== c :
             listhasil = listhasil1
             break
     return listhasil
+
 print(shuffleString('aku',2))
 print(shuffleString('aku',3))
 print(shuffleString('aku',4))
