0225fc809a318418fea8e8f8f44a4deef96532e6
Bagus Prihandono Pamungkas <71998969+bgspamungkas@users.noreply.github.com>
2020-12-29 10:25:23 +0700

    Update Latihan 6.py

--- Tugas8/Latihan 6.py
+++ Tugas8/Latihan 6.py
@@ -3,25 +3,28 @@ nilai = [{'nim' : 'A01', 'nama' : 'Agustina', 'mid' : 50, 'uas' : 80},
          {'nim' : 'A03', 'nama' : 'Chicha', 'mid' : 100, 'uas' : 50}, 
          {'nim' : 'A04', 'nama' : 'Donna', 'mid' : 20, 'uas' : 100},
          {'nim' : 'A05', 'nama' : 'Fatimah', 'mid' : 70, 'uas' : 100}]
-print('=' * 72)
+
+print('=' * 70)
 print('NIM'.ljust(12), end='')
 print('NAMA'.ljust(12), end='')
-print('N. MID'.rjust(8), end='')
-print('N. UAS'.rjust(12), end='')
-print('N. AKHIR'.rjust(12), end='')
+print('Nilai. MID'.rjust(8), end='')
+print('Nilai. UAS'.rjust(12), end='')
+print('Nilai. AKHIR'.rjust(12), end='')
 print('STATUS'.rjust(15))
-print('-' * 72)
-for i in range(len(nilai)):
-    data = nilai[i]
-    nim = data['nim']
-    nama = data['nama']
-    mid = data['mid']
-    uas = data['uas']
+print('-' * 70)
+
+for b in range(len(nilai)):
+    data = nilai [b]
+    nim = data ['nim']
+    nama = data ['nama']
+    mid = data ['mid']
+    uas = data ['uas']
     akhir = (mid + 2*uas)/3
     if akhir >= 60 :
         status = 'LULUS'
     else:
         status = 'TIDAK LULUS'
+        
     print(str(nim).ljust(12), end='')
     print(str(nama).ljust(12),end='')
     print(str(mid).rjust(8), end='')
@@ -29,4 +32,4 @@ for i in range(len(nilai)):
     print(str(round(akhir)).rjust(12),end='')
     print(status.rjust(15))
     
-print('-' * 72)
+print('-' * 70)
